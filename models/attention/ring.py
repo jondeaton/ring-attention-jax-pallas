ring attention query/key/value gradients are correct now!

models/attention/ring.py
@@ -13,12 +13,12 @@ import math
 import functools
 import einops
 
-from jaxtyping import Float, Int, Array, PRNGKeyArray
+from jaxtyping import Float, Int, Array, PRNGKeyArray, PyTree
 
 from einops import rearrange, repeat
 
 
-def rotate_block(x: jax.Array, axis_name: str, axis_size: int) -> jax.Array:
+def rotate_block(x: PyTree, axis_name: str, axis_size: int) -> PyTree:
     """Rotates an array block (ie key/query block) along the axis.
 
     Args:
@@ -64,8 +64,7 @@ def _ring_attention_fwd(
         pv = einops.einsum(p, v, "b h lq lk, b h lk dv -> b h lq dv")
         o = o / jnp.exp(m_prev - m)[..., None] + pv
 
-        k = rotate(k)
-        v = rotate(v)
+        k, v = rotate([k, v])
         return (o, l, m, k, v), None
 
     # Loop state initialization.
@@ -95,43 +94,37 @@ def _ring_attention_bwd(axis_name: str, res, do):
 
     # TODO: get this number without doing a collective op?
     axis_size = jax.lax.psum(1, axis_name)
-
     rotate = functools.partial(rotate_block, axis_name=axis_name, axis_size=axis_size)
 
-    def scan_fn(carry, i: Int):
-        q, k, v, o, dq, dk, dv, do, L = carry
+    sm_scale = 1 / math.sqrt(dim_k)
 
-        s = einops.einsum(q, k, "b h lq dk, b h lk dk -> b h lq lk") / math.sqrt(dim_k)
+    def scan_fn(carry, i: Int):
+        q, o, dq, dk, dv, do, L = carry
+
+        s = einops.einsum(q, k, "b h lq dk, b h lk dk -> b h lq lk") * sm_scale
         p: Float[Array, "b h lq lk"] = jnp.exp(s - L[..., None])
 
         dv += einops.einsum(p, do, "b h lq lk, b h lq dv -> b h lk dv")
-        dp = einops.einsum(do, v, "b h lq dv, b h lk dv -> b h lq lk")
 
-        delta = einops.einsum(do, o, "b h lq dv, b h lq dv -> b h lq")
-        ds: Float[Array, "b h lq lk"] = p * (dp - delta[:, :, :, None])
+        dp = einops.einsum(do, v, "b h lq dv, b h lk dv -> b h lq lk")
+        delta: Float[Array, "b h lq 1"] = jnp.sum(do * o, keepdims=True, axis=-1)
+        ds: Float[Array, "b h lq lk"] = p * (dp - delta) * sm_scale
 
         dq += einops.einsum(ds, k, "b h lq lk, b h lk dk -> b h lq dk")
         dk += einops.einsum(ds, q, "b h lq lk, b h lq dk -> b h lk dk")
 
-        q = rotate(q)
-        dq = rotate(dq)
-        o = rotate(o)
-        do = rotate(do)
-        L = rotate(L)
-
-        return (q, k, v, o, dq, dk, dv, do, L), None
+        q, o, dq, do, L = rotate([q, o, dq, do, L])
+        return (q, o, dq, dk, dv, do, L), None
 
     dq = jnp.zeros(shape=(batch, num_heads, q_len, dim_k), dtype=q.dtype)
     dk = jnp.zeros(shape=(batch, num_heads, kv_len, dim_k), dtype=k.dtype)
     dv = jnp.zeros(shape=(batch, num_heads, kv_len, dim_v), dtype=v.dtype)
 
-    (q, k, v, o, dq, dk, dv, do, _), _ = jax.lax.scan(
+    (q, o, dq, dk, dv, do, _), _ = jax.lax.scan(
         scan_fn,
-        init=(q, k, v, o, dq, dk, dv, do, L),
+        init=(q, o, dq, dk, dv, do, L),
         xs=jnp.arange(axis_size),
     )
-
-    dq = rotate(dq)  # final rotation to get gradients of current block's querries.
 
     dq = einops.rearrange(dq, "b h l d -> b l h d")
     dk = einops.rearrange(dk, "b h l d -> b l h d")

models/attention/ring_test.py
@@ -34,7 +34,7 @@ def mha(
     B, Lq, H, Dk = q.shape
     _, Lk, *_ = k.shape
 
-    qk = einops.einsum(q, k, "b i h d, b j h d -> b h i j ")
+    qk = einops.einsum(q, k, "b i h d, b j h d -> b h i j")
     z = qk / jnp.sqrt(Dk)
     if bias is not None:
         z += bias
@@ -166,23 +166,21 @@ def test_ring_attention_backward():
         check_rep=False,
     )
 
-    def f(q, k, v):
-        o = ring_attention_sharded(q, k, v)
-        return o.sum()
-
     with jax.disable_jit():
-        dq, dk, dv = jax.grad(f, argnums=(0, 1, 2))(q, k, v)
+        dq, dk, dv = jax.grad(
+            lambda q, k, v: ring_attention_sharded(q, k, v).sum(),
+            argnums=(0, 1, 2),
+        )(q, k, v)
 
     assert not jnp.isnan(dq).any()
     assert not jnp.isnan(dk).any()
     assert not jnp.isnan(dv).any()
 
-    def f(q, k, v):
-        o = mha(q, k, v)
-        return o.sum()
+    dq_, dk_, dv_ = jax.grad(
+        lambda q, k, v: mha(q, k, v).sum(),
+        argnums=(0, 1, 2),
+    )(q, k, v)
 
-    dq_, dk_, dv_ = jax.grad(f, argnums=(0, 1, 2))(q, k, v)
-
-    np.testing.assert_allclose(dq, dq_)
-    np.testing.assert_allclose(dk, dk_)
-    np.testing.assert_allclose(dv, dv_)
+    np.testing.assert_allclose(dq, dq_, atol=1e-4)
+    np.testing.assert_allclose(dk, dk_, atol=1e-4)
+    np.testing.assert_allclose(dv, dv_, atol=1e-4)